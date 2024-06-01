b682dbf105720e33255a9464f9054003341fb895
YuheshPandian <yuheshpandian@gmail.com>
2024-06-01 17:05:46 +0530

    improved structure and added home page

--- app/views.py
+++ app/views.py
@@ -1,6 +1,9 @@
+from flask import render_template, url_for
 from app import app
 
 
 @app.route("/")
+@app.route("/home")
+@app.route("/index")
 def index():
-    return "Hello World"
+    return render_template("index.html")
