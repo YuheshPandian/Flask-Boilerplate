aed703e8cfab887f8efc1eac665a656b0964ddc4
YuheshPandian <yuheshpandian@gmail.com>
2024-12-24 14:50:33 +0530

    improved homepage frontend and working on errologger

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,10 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+from .views import app, page_not_found
+
+app.register_error_handler(404, page_not_found)
 app.config.from_pyfile("config.py")
 
 
--- app/views.py
+++ app/views.py
@@ -1,5 +1,13 @@
 from flask import render_template, url_for
 from app import app
+from logging import FileHandler, WARNING
+
+if app.debug:
+    file_handler = FileHandler("errolog.txt")
+    file_handler.setLevel(WARNING)
+
+    app.logger.addHandler(file_handler)
+
 
 
 @app.route("/")
@@ -7,3 +15,12 @@ from app import app
 @app.route("/index")
 def index():
     return render_template("index.html")
+
+@app.route("/r")
+def layout():
+    return render_template("layout.html")
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
